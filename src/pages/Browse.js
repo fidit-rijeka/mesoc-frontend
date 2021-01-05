defacbc6a3a831f64d0e81baae17b7f9d359edc3
Valentin Kuharic <valentinkuharic12@gmail.com>
2021-01-05 16:09:46 +0100

    Added the world map to /browse
    - added title card to /browse
    - added the map to /browse (laeflet, mapbox-gl)
    - app requires .env file (see map.js for explanation)
    - added an example marker with on-click popup
    - added ".map-container" class to myMain.scss
    - added .env rule to gitignore

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Card, CardBody, CardTitle, CardSubtitle, Row, Col, Button } from "reactstrap";
 
 import Sidenav from '../components/sidenav';
+import Map from '../components/map';
 
 const Browse = () => {
   return(
@@ -8,7 +10,15 @@ const Browse = () => {
       <div className="sidenavArea">
         <Sidenav />
       </div>
-      <div className="pageArea">Browse page</div>
+      <div className="pageArea">
+        <Card>
+          <CardBody>
+            <CardTitle>MESOC world map</CardTitle>
+            <CardSubtitle className="mb-3">Explore our MESOC data visualized on a world map.</CardSubtitle>
+            <Map />
+          </CardBody>
+        </Card>
+      </div>
     </div>
   );
 };

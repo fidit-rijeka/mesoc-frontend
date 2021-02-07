0800b10b190d14ba7769b666f64319b2ba35c21b
Valentin Kuharic <valentinkuharic12@gmail.com>
2021-02-07 15:53:27 +0100

    Removed /webapp prefix (not good enough solution, will be redone)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/webapp">
+    <BrowserRouter /*basename=""*/>
       <App />
     </BrowserRouter>
   </React.StrictMode>,

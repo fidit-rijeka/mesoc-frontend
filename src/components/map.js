488aba5b8445c116ed683610942576e66ba9ec32
Valentin331 <valentinkuharic12@gmail.com>
2021-03-06 17:09:45 +0100

    Added marker coloring, fully works, tested

--- src/components/map.js
+++ src/components/map.js
@@ -3,7 +3,12 @@ import Leaflet from 'leaflet';
 import {} from 'mapbox-gl-leaflet';
 import axios from 'axios';
 
-import AnalysisLoader from './analysisLoader';
+import m_blue from '../assets/images/map/markers/blue.png';
+import m_red from '../assets/images/map/markers/red.png';
+import m_purple from '../assets/images/map/markers/purple.png';
+import m_shadow from '../assets/images/map/markers/shadow.png';
+
+//import AnalysisLoader from './analysisLoader';
 
 const Map = () => {
 
@@ -19,6 +24,39 @@ const Map = () => {
       zoom: 4
     };
 
+    var blueIcon = Leaflet.icon({
+      iconUrl: m_blue,
+      shadowUrl: m_shadow,
+  
+      iconSize:     [25, 41], // size of the icon
+      shadowSize:   [43, 25], // size of the shadow
+      iconAnchor:   [13, 40], // point of the icon which will correspond to marker's location
+      shadowAnchor: [5, 25],  // the same for the shadow
+      popupAnchor:  [0, -40] // point from which the popup should open relative to the iconAnchor
+    });
+
+    var redIcon = Leaflet.icon({
+      iconUrl: m_red,
+      shadowUrl: m_shadow,
+
+      iconSize:     [25, 41], // size of the icon
+      shadowSize:   [43, 25], // size of the shadow
+      iconAnchor:   [13, 40], // point of the icon which will correspond to marker's location
+      shadowAnchor: [5, 25],  // the same for the shadow
+      popupAnchor:  [0, -40] // point from which the popup should open relative to the iconAnchor
+    });
+
+    var purpleIcon = Leaflet.icon({
+      iconUrl: m_purple,
+      shadowUrl: m_shadow,
+      
+      iconSize:     [25, 41], // size of the icon
+      shadowSize:   [43, 25], // size of the shadow
+      iconAnchor:   [13, 40], // point of the icon which will correspond to marker's location
+      shadowAnchor: [5, 25],  // the same for the shadow
+      popupAnchor:  [0, -40] // point from which the popup should open relative to the iconAnchor
+    });
+
     const map = Leaflet.map(mapContainer).setView([initialState.lat, initialState.lng], initialState.zoom);
 
     // using map provided from mapbox
@@ -27,23 +65,57 @@ const Map = () => {
       accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
     }).addTo(map);
 
-    // Example of defining a custom marker icon
-    
-    // var leafletIcon = Leaflet.icon({
-    //   iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',
-    //   iconSize: [48,50], // width and height
-    //   iconAnchor: [22,32] // Leaflet renders marker from the top left corner of the image. Use these values to adjust image position
-    // });
-
     axios
       .get('https://api.mesoc.dev/aggregates/location/')
       .then(res => {
-        // TODO:
-        // map through all markers and add them to the map
+
+        // Simulating pilot cities and mixed citites
+        // (at the time of testing there were none, so I'm adding them to test my code)
+        // NOTE => marker coloring works, if required, uncomment the fake data (fake markers are near Rijeka, Croatia)
+        
+        // Purple marker fake data
+        /*
+        res.data.push({
+          city: "test1",
+          country: "wda",
+          latitude: "45.3271",
+          longitude: "14.4422",
+          num_pilot: 2,
+          num_scientific: 5
+        });
+        */
+
+        /*
+        // Red marker test data
+        res.data.push({
+          city: "test1",
+          country: "wda",
+          latitude: "45.4",
+          longitude: "14.8",
+          num_pilot: 2,
+          num_scientific: 0
+        });
+        */
+
         console.log(res.data);
 
+        // Looping through data and setting markers on map
+        // TODO: Is mapping this data inside axios function a bad practice?
         res.data.map(markerData => {
-          Leaflet.marker([markerData.latitude, markerData.longitude]).bindPopup(`
+
+          // Checking the numbers and types of studies and choosing a marker icon accordingly
+          if (markerData.num_pilot == 0 ) {
+            // Only scientific papers (blue marker)
+            var markerIcn = blueIcon;
+          } else if (markerData.num_scientific == 0) {
+            // Only pilot papers (red marker)
+            var markerIcn = redIcon;
+          } else {
+            // both pilot and scientific num is > 0, then it's mixed marker (purple) (other options exist under "assets" folder)
+            var markerIcn = purpleIcon;
+          }
+
+          Leaflet.marker([markerData.latitude, markerData.longitude], {icon: markerIcn}).bindPopup(`
             <p class="mapPopupTitle">${markerData.city}, ${markerData.country}</p>
             ${markerData.num_pilot !== 0 ? `<p>Pilot case studies: ${markerData.num_pilot}</p><a href="/location/${markerData.latitude}-${markerData.longitude}">Examine pilot studies</a>` : ''}
             ${markerData.num_scientific !== 0 ? `<p>Scientific case studies: ${markerData.num_scientific}</p><a href="/location/${markerData.latitude}-${markerData.longitude}">Examine scientific studies</a>` : ''}
@@ -52,11 +124,6 @@ const Map = () => {
         
         setMapLoaded(true);
       })
-
-    // Example marker (coordinates of rijeka)
-    //const mrk = Leaflet.marker([45.3271, 14.4422]).bindPopup(`<a href="https://www.mesoc-project.eu/">Example link</a>`).addTo(map);
-    Leaflet.marker([45.3271, 14.4422]).bindPopup(`<a href="https://www.mesoc-project.eu/">Example link</a>`).addTo(map);
-
   }, [mapContainer]);
 
   return (

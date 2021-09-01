341f8efa37a6ffd31159b5dc24009e00b5fd28ce
Erik Jermaniš <erik55jermanis@gmail.com>
2021-09-01 22:29:08 +0200

    adding aggregate changes

--- src/pages/Analysis.js
+++ src/pages/Analysis.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Card, CardBody, CardTitle, CardSubtitle, Row, Col, Button } from "reactstrap";
+import { Card, CardBody, CardTitle, CardSubtitle, Row, Col, Button, Modal } from "reactstrap";
 import axios from 'axios';
 
 import Sidenav from '../components/sidenav';
@@ -23,6 +23,35 @@ const compare = (a, b) => {
   return comparison;
 };
 
+const KeywordsModal = ({ isOpen, setIsOpen, data }) => {
+
+  useEffect(() => {
+    console.log(data.keywords);
+  }, []);
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      centered
+    >
+      <div className="modal-header">
+        <h5 className="modal-title mt-0">Impact keywords</h5>
+      </div>
+      <div className="modal-body">
+        <p>
+          {
+            data.keywords &&
+              data.keywords.map((each, index) => ( <span>{each}, </span> ))
+          }
+        </p>
+      </div>
+      <div className="modal-footer">
+        <button onClick={() => setIsOpen(false)} className="btn btn-secondary wawes-effect">Close</button>
+      </div>
+    </Modal>
+  )
+};
+
 const Analysis = ({ userToken, match }) => {
 
   const [cells, setCells] = useState(null);
@@ -32,6 +61,8 @@ const Analysis = ({ userToken, match }) => {
   const [selectedVar, setSelectedVar] = useState(null);
   const [varSim, setVarSim] = useState(null);
   const [cellSimLoading, setCellSimLoading] = useState(false);
+  const [varSimLoading, setVarSimLoading] = useState(false);
+  const [isKeywordsModal, setIsKeywordsModal] = useState(false);
 
   useEffect(() => {
     if(match.params.analysisType === 'location') {
@@ -41,7 +72,7 @@ const Analysis = ({ userToken, match }) => {
       axios
         .get(`https://api.mesoc.dev/aggregates/heatmap/?latitude=${latlong[0]}&longitude=${latlong[1]}&type=${latlong[2]}`)
         .then(async res => {
-          console.log(res.data);
+          console.log(res);
           let heatData = await [...Array(30).keys()].map(x => {return {order: x, classification: 0.0}});
           await res.data.map(resItem => {
             heatData[resItem.order] = resItem;
@@ -73,9 +104,7 @@ const Analysis = ({ userToken, match }) => {
     }
 
     // Reset graph data and similar by cell data.
-    if(match.params.analysisType !== 'location') {
-      setVars(null);
-    }
+    setVars(null);
     setCellSim(null);
 
     // If deselecting cell or clicking on "classification: 0" cell, clear cell data and terminate function.
@@ -84,11 +113,12 @@ const Analysis = ({ userToken, match }) => {
       return;
     }
 
+    const latlong = match.params.analysisKey.split('_');
+
     // Mark selected cell.
     setSelectedCell(cellIndex);
     setCellSimLoading(true);
     if(match.params.analysisType === 'location') {
-      const latlong = match.params.analysisKey.split('_');
       axios
         .get(`https://api.mesoc.dev/aggregates/similar/cell/?latitude=${latlong[0]}&longitude=${latlong[1]}&cell=${cellIndex}`)
         .then(res => {
@@ -107,7 +137,6 @@ const Analysis = ({ userToken, match }) => {
           setCellSimLoading(false);
         });
     }
-    
 
     if (match.params.analysisType !== 'location') {
       // Fetch graph data.
@@ -115,48 +144,88 @@ const Analysis = ({ userToken, match }) => {
       // Sort graph data by strength.
       await graphData.sort(compare);
       // Modify graph data for displaying.
-      await graphData.map(barData => {
-        barData.strength = barData.strength * 100;
-        barData.Strength = 0;
-      })
+      await graphData.map(barData => { barData.strength = Math.round(barData.strength * 100) });
       // Set graph data to state.
       setVars(graphData);
+    } else {
+      let cellNum;
+      if(cellIndex.toString().length === 1)
+        cellNum = 0;
+      else if(Number(cellIndex.toString().charAt(0)) === 1)
+        cellNum = 1;
+      else
+        cellNum = 2;
+
+      const result = await (await axios.get(`https://api.mesoc.dev/aggregates/impact/?type=${latlong[2]}&latitude=${latlong[0]}&longitude=${latlong[1]}&column=${cellNum}`, {
+        headers: {
+          Authorization: `Bearer ${userToken}`
+        }
+      })).data;
+      await result.sort(compare);
+      await result.map(barData => { barData.strength = Math.round(barData.strength * 100) });
+      console.log(result);
+      setVars(result);
     }
   };
 
-  const graphClick = async data => {
+  const graphClick = async varIndex => {
     setVarSim(null);
 
-    // Create a copy of graph data.
-    let newGraphData = [...vars];
-
-    if(data.id === 'strength') {
-      // Bar is being selected.
-      await newGraphData.map(current => {
-        if(current.Strength) {
-          current.strength = current.Strength;
-          current.Strength = 0;
-        }
-      });
-      newGraphData[data.index].Strength = newGraphData[data.index].strength;
-      newGraphData[data.index].strength = 0;
-
-      setSelectedVar(data.id);
-
-      // TODO: fetch similar by variable
-      setTimeout(() => {
-        setVarSim(require('../testData/simm.json').similar);
-      }, 1000);
-    } else {
-      // Bar is being deselected.
-      newGraphData[data.index].strength = newGraphData[data.index].Strength;
-      newGraphData[data.index].Strength = 0;
-      
+    if(selectedVar === varIndex || vars[varIndex].strength === 0) {
       setSelectedVar(null);
+      return;
     }
-    // Set modified graph data to state.
-    setVars(newGraphData);
-  }
+
+    const latlong = match.params.analysisKey.split('_');
+
+    setVarSimLoading(true);
+    setSelectedVar(varIndex);
+
+    let cellNum;
+      if(selectedCell.toString().length === 1)
+        cellNum = 0;
+      else if(Number(selectedCell.toString().charAt(0)) === 1)
+        cellNum = 1;
+      else
+        cellNum = 2;
+
+    const result = await axios.get(`https://api.mesoc.dev/aggregates/similar/cell/?latitude=${latlong[0]}&longitude=${latlong[1]}&cell=${cellNum}`)
+    setVarSim(result.data);
+    console.log(result.data);
+    setVarSimLoading(false)
+
+    // // Create a copy of graph data.
+    // let newGraphData = [...vars];
+
+    // if(data.id === 'strength') {
+    //   // Bar is being selected.
+    //   await newGraphData.map(current => {
+    //     if(current.Strength) {
+    //       current.strength = current.Strength;
+    //       current.Strength = 0;
+    //     }
+    //   });
+    //   newGraphData[data.index].Strength = newGraphData[data.index].strength;
+    //   newGraphData[data.index].strength = 0;
+
+    //   setSelectedVar(data.id);
+
+    //   // TODO: fetch similar by variable
+    //   setTimeout(() => {
+    //     setVarSim(require('../testData/simm.json').similar);
+    //   }, 1000);
+    // } else {
+    //   // Bar is being deselected.
+    //   newGraphData[data.index].strength = newGraphData[data.index].Strength;
+    //   newGraphData[data.index].Strength = 0;
+      
+    //   setSelectedVar(null);
+    // }
+    // // Set modified graph data to state.
+    // setVars(newGraphData);
+  };
+
+  const displayImpactKeywords = () => {};
 
   return(
     <div className="pageWrapper">
@@ -164,6 +233,13 @@ const Analysis = ({ userToken, match }) => {
         <Sidenav />
       </div>
       <div className="pageArea">
+
+        <KeywordsModal
+          isOpen={isKeywordsModal}
+          setIsOpen={setIsKeywordsModal}
+          data={(selectedVar !== null && vars !== null) ? vars[selectedVar] : {}}
+        />
+
         <Row>
           <Col xl="5" lg="12">
             <Card>
@@ -190,15 +266,20 @@ const Analysis = ({ userToken, match }) => {
               <CardBody>
                 <CardTitle>MESOC Graph</CardTitle>
                 <CardSubtitle className="mb-3">Distribution of variables impacting selected cell</CardSubtitle>
-                {selectedCell !== null ?
+                {/* {selectedCell !== null ?
                   match.params.analysisType !== 'location' ?
                     <Graph vars={vars} varClick={graphClick} /> :
                     <div className="analysisEmpty" style={{ height: '550px' }}>{match.params.analysisType === 'location' ? 'Feature coming soon' : 'No cell selected'}</div>:
-                    <div className="analysisEmpty" style={{ height: '550px' }}>Feature coming soon</div>
+                  <div className="analysisEmpty" style={{ height: '550px' }}>Feature coming soon</div>
+                } */}
+                {selectedCell !== null ?
+                  <Graph vars={vars} varClick={graphClick} selectedVar={selectedVar} /> :
+                  <div className="analysisEmpty" style={{ height: '550px' }}>No cell selected</div>
                 }
                 {/* TODO:
                 Fetch data and display variable decomposition screen (as a modal) on click of this button. */}
-                <Button onClick={() => console.log(selectedVar)} color="secondary" className="ml-3" disabled={selectedVar === null}>See decomposition</Button>
+                {/* <Button onClick={() => console.log(selectedVar)} color="secondary" className="ml-3" disabled={selectedVar === null}>See decomposition</Button> */}
+                <Button onClick={() => setIsKeywordsModal(true)} color="secondary" disabled={selectedVar === null}>See keywords</Button>
               </CardBody>
             </Card>
           </Col>
@@ -228,12 +309,25 @@ const Analysis = ({ userToken, match }) => {
                   <CardBody>
                     <CardTitle>Similar documents</CardTitle>
                     <CardSubtitle className="mb-3">Document similarity by selected variable</CardSubtitle>
-                    {selectedVar !== null ?
-                      varSim.length ? 
-                        <DocumentList docs={varSim} /> :
-                        <div className="analysisEmpty" style={{ height: '200px' }}>No similar documents</div> :
-                        <div className="analysisEmpty" style={{ height: '200px' }}>No variable selected</div>
+                    {
+                      selectedVar !== null ?
+                      (varSimLoading ?
+                        <AnalysisLoader height="200px" /> :
+                        (varSim.length ?
+                          <DocumentList docs={varSim} /> :
+                          <div className="analysisEmpty" style={{ height: '200px' }}>No similar documents</div>)) :
+                      <div className="analysisEmpty" style={{ height: '200px' }}>No cell selected</div>
                     }
+                    {/* {
+                      selectedVar !== null ?
+                    }
+                    {
+                      selectedVar !== null ?
+                        varSim.length ? 
+                          <DocumentList docs={varSim} /> :
+                          <div className="analysisEmpty" style={{ height: '200px' }}>No similar documents</div> :
+                        <div className="analysisEmpty" style={{ height: '200px' }}>No variable selected</div>
+                    } */}
                   </CardBody>
                 </Card>
               </Col>
